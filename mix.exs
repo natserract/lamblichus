5dc05542ccce61fb2e77eae1fd8c30241ee69c11
natserract <benjaminstwo@gmail.com>
2022-05-03 17:47:35 +0800

    feat: 🎸 pubslished packages to hex.pm

--- mix.exs
+++ mix.exs
@@ -7,6 +7,8 @@ defmodule Lamblichus.MixProject do
       version: "0.1.0",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
+      description: description(),
+      package: package(),
       deps: deps()
     ]
   end
@@ -21,8 +23,22 @@ defmodule Lamblichus.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
+    ]
+  end
+
+  defp description do
+    """
+    Lamblichus is a elixir macro for implemented functor, like in haskell
+    """
+  end
+
+  defp package do
+    [
+      files: ["lib", "mix.exs", "README*", "LICENSE*"],
+      maintainers: ["Alfin Surya"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/natserract/lamblichus"}
     ]
   end
 end
